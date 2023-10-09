99402d06ee6ff39a1bf084c3be468a8a3dd87d77
Vishal Bhapkar <vishalbhapkar@vishalbhap-ventura.local>
2023-10-09 18:49:59 +0530

    update yogesh kore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  const incrementCount = () => {-
+  const incrementCount = () => {
     setCount(count + 1);
   };
 
